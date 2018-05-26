8d1b90c455db0a4bd06da4d16a2cf2ca0fb9d4e2
Nick Wilson <logikfx@gmail.com>
2018-05-26 16:00:27 -0400

    basic command line args program added, may be useful for reference later on

---
+++ commandlineargs/CommandLineArgs.java
@@ -0,0 +1,13 @@
+/** CommandLineArgs.java
+ *  Echo command line args back.
+ *  Just used for reference.
+ */
+
+public class CommandLineArgs {
+
+    public static void main (String[] args) {
+        for(String s: args) {
+            System.out.println(s);
+        }
+    }
+}
